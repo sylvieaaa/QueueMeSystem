5c9eb274d7361f89515bb378186c3175dca1cabd
Low Rui Jia <lowruijia96@gmail.com>
2018-03-21 21:25:18 +0800

    - MenuTypeEnum and VendorTypeEnum

---
+++ QueueMeSystem/QueueMeSystemLibrary/src/util/enumeration/MenuTypeEnum.java
@@ -0,0 +1,18 @@
+package util.enumeration;
+
+public enum MenuTypeEnum {
+    MAINS,
+    SIDES,
+    STIR_FRY,
+    STEAM,
+    FRIED,
+    APPETIZERS,
+    SALAD,
+    DESSERT,
+    STARTER,
+    GRILL,
+    SOUP,
+    NOODLES,
+    SNACKS,
+    PASTRY
+}
---
+++ QueueMeSystem/QueueMeSystemLibrary/src/util/enumeration/VendorTypeEnum.java
@@ -0,0 +1,26 @@
+package util.enumeration;
+
+public enum VendorTypeEnum {
+    WESTERN,
+    JAPANESE,
+    THAI,
+    VIETNAMESE,
+    FRENCH,
+    KOREAN,
+    SPANISH,
+    PERANAKAN,
+    ITALIAN,
+    GERMAN,
+    MEXICAN,
+    CHINESE,
+    MEDITERRANEAN,
+    SEAFOOD,
+    DRINKS,
+    FRUITS,
+    MALAY,
+    LOCAL_DELIGHTS,
+    INDIAN,
+    GREEK,
+    HALAL,
+    VEGETARIAN
+}
