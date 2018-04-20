736e7cfe56e75099b6760a96d42fcdd562ae9c58
Soong Yi Xuan <yxsoong@gmail.com>
2018-04-20 16:58:40 +0800

    Menu Item Entity edited

--- QueueMeSystem/QueueMeSystem-ejb/src/java/entity/MenuItemEntity.java
+++ QueueMeSystem/QueueMeSystem-ejb/src/java/entity/MenuItemEntity.java
@@ -10,6 +10,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -31,6 +32,7 @@ public class MenuItemEntity implements Serializable {
     private Long menuItemId;
     private String menuItemName;
     private String description;
+    @Column(precision = 11, scale = 2)
     private BigDecimal price;
     private String photoURL;
     private Boolean enabled;
